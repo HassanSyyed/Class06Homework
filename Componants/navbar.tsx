38496ce0ac07b5337eea5c5fcefcbcddb9a208e3
Syed Hassan Imam <hassan.syyed@yahoo.com>
2024-10-31 17:37:02 +0500

    Add files via upload

---
+++ Componants/navbar.tsx
@@ -0,0 +1,48 @@
+
+"use client"
+import React from "react"
+import Link from "next/link"
+export default function Navbar() {
+    return (
+
+        <div className="flex flex-col justify-center">
+            <nav className=" px-4 flex justify-center items-center flex-row w-full text-white font-medium bg-slate-900">
+                <div className="mb-4 sm:mb-0" >
+                    <img className=" px-3 py-3 h-auto w-56 text-lg sm:text-xl md:text-2xl lg:text-2xl" src="https://wsa1.pakwheels.com/assets/new-pw-logo-white-c4cd16ae34613cc1fa16d7840f34a71e.svg" alt="logo" />
+                </div>
+                < div className="w-full sm:w-auto mb-4 sm:mb-0">
+ 
+                    <ul className="flex flex-row ">
+                        <li><Link href="/banner" className="hidden lg:inline hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">UsedCars</Link></li>
+                        <li><Link href="/Featured" className=" hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">New Cars</Link></li>
+                        <li><Link href="/bikes" className="hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">Bikes</Link></li>
+                         <li><Link href="/autostore" className="hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">Auto store</Link></li>
+                        <li><Link href="/vidios" className=" hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">Videos</Link></li>
+                        <li><Link href="/forms" className="hidden lg:inline hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">Forms</Link></li>
+                        <li><Link href="/Footer" className="hidden lg:inline hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium" >Blog</Link></li>
+                        <li><Link href="/Order" className="hidden lg:inline hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">More</Link></li>
+
+                    </ul>                        
+                </div>
+                
+                <div>
+                    <ul className="flex flex-row gap-2">
+                        <Link href="/login" className="hidden lg:inline hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">Sign In</Link>
+                        <Link href="/register" className="hidden lg:inline hover:text-red-600 hover:bg-white px-3 py-2 rounded-md text-sm font-medium">Sign Up</Link>
+                        <Link href="/post-ad" className="bg-red-600 hover:bg-red-800 rounded-md text-sm font-medium px-3 py-2 ">Post an Ad</Link>
+                    </ul>
+                </div>
+
+
+            </nav>
+
+
+        </div>
+
+    )
+}
+
+    
+
+    
+    
