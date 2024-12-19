261ea8bc5cfa46f5673eb60294dcc5fdd0c6ff4a
Syed Hassan Imam <hassan.syyed@yahoo.com>
2024-12-19 12:59:18 +0500

    My Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import Navbar from'./Componants/navbar';
-import Banner from './Banner/page';
-import HeroSection from './Herosection/page';
-import Featured from './Featured/page';
-import Booking from './Booking/page';
-import Footer from './Footer/page';
+import Navbar from "./Components/navbar";
+import Banner from "./Banner/page";
+import HeroSection from "./Herosection/page";
+import Featured from "./Featured/page";
+import Booking from "./Booking/page";
+import Footer from "./Footer/page";
 
 export default function Home() {
   return (
